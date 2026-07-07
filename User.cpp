45b5abdca40ea422940f31ba64dab6f9d6bd7488
zcy244 <811319607@qq.com>
2026-07-07 16:49:07 +0800

    Add files via upload

---
+++ User.cpp
@@ -0,0 +1,30 @@
+#ifndef USER_H
+#define USER_H
+
+#include <string>
+#include "enums.h"
+
+using namespace std;
+
+
+class User
+{
+private:
+    string username;
+    string password;
+    Role role;
+    string realName;
+public:
+    User(string u, string p, Role r, string n);
+
+    string getUsername() const;
+    Role getRole() const;
+    string getName() const;
+
+    bool changePwd(string oldPwd, string newPwd);
+    bool checkPwd(string pwd) const;
+    bool isSuperAdmin() const;
+    void showInfo() const;
+    string toFileStr() const;
+};
+#endif
